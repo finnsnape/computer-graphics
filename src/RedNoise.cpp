0c0149bb7351b989b4077df91aee7436187318f9
finnsnape <snapefinn@gmail.com>
2022-11-25 23:40:07 +0000

    Switched to ray tracing
    Temporarily disabled event handling

--- src/RedNoise.cpp
+++ src/RedNoise.cpp
@@ -1,62 +1,58 @@
-#include <CanvasTriangle.h>
 #include <DrawingWindow.h>
 #include <CanvasPoint.h>
 #include <ModelTriangle.h>
 #include <glm/glm.hpp>
 #include <vector>
-#include "utils/Triangles.h"
-#include "utils/Files.h"
-#include "utils/Camera.h"
+#include "Triangles.h"
+#include "Files.h"
+#include "Camera.h"
 
-#define WIDTH 480 // 320
-#define HEIGHT 480 // 240
+#define WIDTH 480
+#define HEIGHT 480
 
-void draw(DrawingWindow &window, Camera &camera, const std::vector<ModelTriangle>& modelTriangles) {
+void drawRayTraced(DrawingWindow &window, Camera &camera, std::vector<ModelTriangle> &modelTriangles) {
     window.clearPixels();
-    camera.resetDepthBuffer();
-    for (auto modelTriangle : modelTriangles) {
-        std::vector<CanvasPoint> rawTriangle;
-        for (int j = 0; j < 3; j++) {
-            glm::vec3 vertex = modelTriangle.vertices[j];
-            CanvasPoint intersectionPoint = camera.getIntersectionPoint(vertex);
-            rawTriangle.push_back(intersectionPoint);
+    for (int x=0; x<WIDTH; x++) {
+        for (int y=0; y<HEIGHT; y++) {
+            CanvasPoint canvasPoint((float) x, (float) y);
+            glm::vec3 rayDirection = camera.getRayDirection(canvasPoint);
+            RayTriangleIntersection solution = camera.getClosestIntersection(modelTriangles, rayDirection);
+            if (solution.exists) {
+                drawPixel(window, canvasPoint, solution.intersectedTriangle.colour);
+            }
         }
-        CanvasTriangle triangle({rawTriangle[0], rawTriangle[1], rawTriangle[2]});
-        drawFilledTriangle(window, camera.depthBuffer, triangle, modelTriangle.colour);
     }
-    camera.orbit(x, 1, {0.0, 0.0, 0.0});
 }
 
 void handleEvent(SDL_Event event, DrawingWindow &window, Camera &camera) {
-	if (event.type == SDL_KEYDOWN) {
-		if (event.key.keysym.sym == SDLK_LEFT) {
-            camera.translate(x, -1);
-        } else if (event.key.keysym.sym == SDLK_RIGHT) {
-            camera.translate(x, 1);
-        } else if (event.key.keysym.sym == SDLK_UP) {
-            camera.translate(y, 1);
-        } else if (event.key.keysym.sym == SDLK_DOWN) {
-            camera.translate(y, -1);
-        } else if (event.key.keysym.sym == SDLK_MINUS) {
-            camera.translate(z, 1);
-        } else if (event.key.keysym.sym == SDLK_EQUALS) {
-            camera.translate(z, -1);
-        } else if (event.key.keysym.sym == SDLK_w) {
-            camera.rotate(x, -1);
-        } else if (event.key.keysym.sym == SDLK_s) {
-            camera.rotate(x, 1);
-        } else if (event.key.keysym.sym == SDLK_a) {
-            camera.rotate(y, -1);
-        } else if (event.key.keysym.sym == SDLK_d) {
-            camera.rotate(y, 1);
-        }
-        camera.lookAt({0.0, 0.0, 0.0});
-    }
+//	if (event.type == SDL_KEYDOWN) {
+//		if (event.key.keysym.sym == SDLK_LEFT) {
+//            camera.translate(x, -1);
+//        } else if (event.key.keysym.sym == SDLK_RIGHT) {
+//            camera.translate(x, 1);
+//        } else if (event.key.keysym.sym == SDLK_UP) {
+//            camera.translate(y, 1);
+//        } else if (event.key.keysym.sym == SDLK_DOWN) {
+//            camera.translate(y, -1);
+//        } else if (event.key.keysym.sym == SDLK_MINUS) {
+//            camera.translate(z, 1);
+//        } else if (event.key.keysym.sym == SDLK_EQUALS) {
+//            camera.translate(z, -1);
+//        } else if (event.key.keysym.sym == SDLK_w) {
+//            camera.rotate(x, -1);
+//        } else if (event.key.keysym.sym == SDLK_s) {
+//            camera.rotate(x, 1);
+//        } else if (event.key.keysym.sym == SDLK_a) {
+//            camera.rotate(y, -1);
+//        } else if (event.key.keysym.sym == SDLK_d) {
+//            camera.rotate(y, 1);
+//        }
+//        camera.lookAt({0.0, 0.0, 0.0});
+//    }
 }
 
 int main(int argc, char *argv[]) {
-    // note: we are using a right hand coordinate system
-    float scaleFactor = 0.35; // 0.35
+    float scaleFactor = 0.35;
     glm::vec3 initialPosition(0.0, 0.0, 4.0);
     float focalLength = 2.0;
     Camera camera(initialPosition, focalLength);
@@ -66,7 +62,7 @@ int main(int argc, char *argv[]) {
     while (true) {
         // We MUST poll for events - otherwise the window will freeze !
         if (window.pollForInputEvents(event)) handleEvent(event, window, camera);
-        draw(window, camera, modelTriangles);
+        drawRayTraced(window, camera, modelTriangles);
         // Need to render the frame at the end, or nothing actually gets shown on the screen !
         window.renderFrame();
 	}
