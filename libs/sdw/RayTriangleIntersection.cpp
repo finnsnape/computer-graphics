9c312f9886a958e38c43cbe5a045e1bd8a42693c
finnsnape <snapefinn@gmail.com>
2022-11-25 23:37:48 +0000

    Added exists boolean for ray triangle intersection
    Some rays have no intersection

--- libs/sdw/RayTriangleIntersection.cpp
+++ libs/sdw/RayTriangleIntersection.cpp
@@ -1,7 +1,8 @@
 #include "RayTriangleIntersection.h"
 
 RayTriangleIntersection::RayTriangleIntersection() = default;
-RayTriangleIntersection::RayTriangleIntersection(const glm::vec3 &point, float distance, const ModelTriangle &triangle, size_t index) :
+RayTriangleIntersection::RayTriangleIntersection(bool e, const glm::vec3 &point, float distance, const ModelTriangle &triangle, size_t index) :
+        exists(e),
 		intersectionPoint(point),
 		distanceFromCamera(distance),
 		intersectedTriangle(triangle),
@@ -10,6 +11,6 @@ RayTriangleIntersection::RayTriangleIntersection(const glm::vec3 &point, float d
 std::ostream &operator<<(std::ostream &os, const RayTriangleIntersection &intersection) {
 	os << "Intersection is at [" << intersection.intersectionPoint[0] << "," << intersection.intersectionPoint[1] << "," <<
 	   intersection.intersectionPoint[2] << "] on triangle " << intersection.intersectedTriangle <<
-	   " at a distance of " << intersection.distanceFromCamera;
+	   " at a distance of " << intersection.distanceFromCamera << ", with exist status: " << intersection.exists;
 	return os;
 }
--- libs/sdw/RayTriangleIntersection.h
+++ libs/sdw/RayTriangleIntersection.h
@@ -5,12 +5,13 @@
 #include "ModelTriangle.h"
 
 struct RayTriangleIntersection {
+    bool exists;
 	glm::vec3 intersectionPoint;
 	float distanceFromCamera;
 	ModelTriangle intersectedTriangle;
 	size_t triangleIndex;
 
 	RayTriangleIntersection();
-	RayTriangleIntersection(const glm::vec3 &point, float distance, const ModelTriangle &triangle, size_t index);
+	RayTriangleIntersection(bool e, const glm::vec3 &point, float distance, const ModelTriangle &triangle, size_t index);
 	friend std::ostream &operator<<(std::ostream &os, const RayTriangleIntersection &intersection);
 };
