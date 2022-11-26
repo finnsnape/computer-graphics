a9f90d57383bbfd68ebead9565286f363e57b0cd
finnsnape <snapefinn@gmail.com>
2022-11-26 22:25:39 +0000

    Using namespace, added WIP old rasterised draw function

--- src/RedNoise.cpp
+++ src/RedNoise.cpp
@@ -19,11 +19,28 @@ void drawRayTraced(Scene &scene) {
             CanvasPoint canvasPoint((float) x, (float) y);
             RayTriangleIntersection closestTriangle = RayTracingUtils::findClosestTriangle(scene, canvasPoint);
             if (closestTriangle.distanceFromCamera != FLT_MAX) {
-                drawPixel(scene.window, canvasPoint, closestTriangle.intersectedTriangle.colour);
+                TriangleUtils::drawPixel(scene.window, canvasPoint, closestTriangle.intersectedTriangle.colour);
             }
         }
     }
 }
+
+//void drawRasterised(Scene &scene) {
+//    scene.window.clearPixels();
+//    scene.camera.resetDepthBuffer();
+//    for (auto triangle : scene.triangles) {
+//        // transpose triangle
+//        //
+//        std::vector<CanvasPoint> rawTriangle;
+//        for (int j = 0; j < 3; j++) {
+//            glm::vec3 vertex = modelTriangle.vertices[j];
+//            CanvasPoint intersectionPoint = camera.getIntersectionPoint(vertex);
+//            rawTriangle.push_back(intersectionPoint);
+//        }
+//        CanvasTriangle triangle({rawTriangle[0], rawTriangle[1], rawTriangle[2]});
+//        drawFilledTriangle(window, camera.depthBuffer, triangle, modelTriangle.colour);
+//    }
+//}
 
 void handleEvent(SDL_Event event) {
     //std::cout << event.type << std::endl;
@@ -34,7 +51,7 @@ int main(int argc, char *argv[]) {
     glm::vec3 initialPosition(0.0, 0.0, 4.0);
     float focalLength = 2.0;
     Camera camera(WIDTH, HEIGHT, focalLength, initialPosition);
-    std::vector<ModelTriangle> triangles = loadOBJ(scaleFactor, "cornell-box.obj");
+    std::vector<ModelTriangle> triangles = FilesUtils::loadOBJ(scaleFactor, "cornell-box.obj", "cornell-box.mtl");
     Scene scene(WIDTH, HEIGHT, triangles, camera);
     SDL_Event event;
     while(true) {
