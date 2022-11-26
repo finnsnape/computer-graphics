64ea4a9217a093ab0816b000f998374211c95d2b
finnsnape <snapefinn@gmail.com>
2022-11-26 18:41:05 +0000

    Changed window size to 240x240

--- src/RedNoise.cpp
+++ src/RedNoise.cpp
@@ -7,8 +7,8 @@
 #include "Files.h"
 #include "Camera.h"
 
-#define WIDTH 480
-#define HEIGHT 480
+#define WIDTH 240
+#define HEIGHT 240
 
 void drawRayTraced(DrawingWindow &window, Camera &camera, std::vector<ModelTriangle> &modelTriangles) {
     window.clearPixels();
@@ -24,31 +24,8 @@ void drawRayTraced(DrawingWindow &window, Camera &camera, std::vector<ModelTrian
     }
 }
 
-void handleEvent(SDL_Event event, DrawingWindow &window, Camera &camera) {
-//	if (event.type == SDL_KEYDOWN) {
-//		if (event.key.keysym.sym == SDLK_LEFT) {
-//            camera.translate(x, -1);
-//        } else if (event.key.keysym.sym == SDLK_RIGHT) {
-//            camera.translate(x, 1);
-//        } else if (event.key.keysym.sym == SDLK_UP) {
-//            camera.translate(y, 1);
-//        } else if (event.key.keysym.sym == SDLK_DOWN) {
-//            camera.translate(y, -1);
-//        } else if (event.key.keysym.sym == SDLK_MINUS) {
-//            camera.translate(z, 1);
-//        } else if (event.key.keysym.sym == SDLK_EQUALS) {
-//            camera.translate(z, -1);
-//        } else if (event.key.keysym.sym == SDLK_w) {
-//            camera.rotate(x, -1);
-//        } else if (event.key.keysym.sym == SDLK_s) {
-//            camera.rotate(x, 1);
-//        } else if (event.key.keysym.sym == SDLK_a) {
-//            camera.rotate(y, -1);
-//        } else if (event.key.keysym.sym == SDLK_d) {
-//            camera.rotate(y, 1);
-//        }
-//        camera.lookAt({0.0, 0.0, 0.0});
-//    }
+void handleEvent(SDL_Event event) {
+    std::cout << event.type << std::endl;
 }
 
 int main(int argc, char *argv[]) {
@@ -59,11 +36,9 @@ int main(int argc, char *argv[]) {
     DrawingWindow window = DrawingWindow(WIDTH, HEIGHT, false);
     SDL_Event event;
     std::vector<ModelTriangle> modelTriangles = loadOBJ(scaleFactor, "cornell-box.obj");
-    while (true) {
-        // We MUST poll for events - otherwise the window will freeze !
-        if (window.pollForInputEvents(event)) handleEvent(event, window, camera);
+    while(true) {
+        if (window.pollForInputEvents(event)) handleEvent(event);
         drawRayTraced(window, camera, modelTriangles);
-        // Need to render the frame at the end, or nothing actually gets shown on the screen !
         window.renderFrame();
-	}
+    }
 }
