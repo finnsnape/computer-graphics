fb9151bb10afc359de000efa9ec1a2ead3c3d9c9
finnsnape <snapefinn@gmail.com>
2022-11-26 21:53:03 +0000

    Updated main file, now using new classes

--- src/RedNoise.cpp
+++ src/RedNoise.cpp
@@ -3,42 +3,43 @@
 #include <ModelTriangle.h>
 #include <glm/glm.hpp>
 #include <vector>
-#include "Triangles.h"
-#include "Files.h"
 #include "Camera.h"
+#include "Scene.h"
+#include "TriangleUtils.h"
+#include "FilesUtils.h"
+#include "RayTracingUtils.h"
 
-#define WIDTH 240
-#define HEIGHT 240
+#define WIDTH 480
+#define HEIGHT 480
 
-void drawRayTraced(DrawingWindow &window, Camera &camera, std::vector<ModelTriangle> &modelTriangles) {
-    window.clearPixels();
-    for (int x=0; x<WIDTH; x++) {
-        for (int y=0; y<HEIGHT; y++) {
+void drawRayTraced(Scene &scene) {
+    scene.window.clearPixels();
+    for (int x=0; x<scene.width; x++) {
+        for (int y=0; y<scene.height; y++) {
             CanvasPoint canvasPoint((float) x, (float) y);
-            glm::vec3 rayDirection = camera.getRayDirection(canvasPoint);
-            RayTriangleIntersection solution = camera.getClosestIntersection(modelTriangles, rayDirection);
-            if (solution.exists) {
-                drawPixel(window, canvasPoint, solution.intersectedTriangle.colour);
+            RayTriangleIntersection closestTriangle = RayTracingUtils::findClosestTriangle(scene, canvasPoint);
+            if (closestTriangle.distanceFromCamera != FLT_MAX) {
+                drawPixel(scene.window, canvasPoint, closestTriangle.intersectedTriangle.colour);
             }
         }
     }
 }
 
 void handleEvent(SDL_Event event) {
-    std::cout << event.type << std::endl;
+    //std::cout << event.type << std::endl;
 }
 
 int main(int argc, char *argv[]) {
     float scaleFactor = 0.35;
     glm::vec3 initialPosition(0.0, 0.0, 4.0);
     float focalLength = 2.0;
-    Camera camera(initialPosition, focalLength);
-    DrawingWindow window = DrawingWindow(WIDTH, HEIGHT, false);
+    Camera camera(WIDTH, HEIGHT, focalLength, initialPosition);
+    std::vector<ModelTriangle> triangles = loadOBJ(scaleFactor, "cornell-box.obj");
+    Scene scene(WIDTH, HEIGHT, triangles, camera);
     SDL_Event event;
-    std::vector<ModelTriangle> modelTriangles = loadOBJ(scaleFactor, "cornell-box.obj");
     while(true) {
-        if (window.pollForInputEvents(event)) handleEvent(event);
-        drawRayTraced(window, camera, modelTriangles);
-        window.renderFrame();
+        if (scene.window.pollForInputEvents(event)) handleEvent(event);
+        drawRayTraced(scene);
+        scene.window.renderFrame();
     }
 }
