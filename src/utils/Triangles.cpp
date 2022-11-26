91d652328bb8b390e60e449836b490a05c3f041b
finnsnape <snapefinn@gmail.com>
2022-11-26 18:44:09 +0000

    Triangles utils type changes

--- src/utils/Triangles.cpp
+++ src/utils/Triangles.cpp
@@ -5,10 +5,10 @@
 /* Drawing */
 
 void drawPixel(DrawingWindow &window, CanvasPoint point, Colour colour) {
-    int x = ceil(point.x);
-    int y = ceil(point.y);
+//    int x = ceil(point.x);
+//    int y = ceil(point.y);
     uint32_t colourCode = (255 << 24) + (colour.red << 16) + (colour.green << 8) + colour.blue;
-    window.setPixelColour(x, y, colourCode);
+    window.setPixelColour(point.x, point.y, colourCode);
 }
 
 /* Triangles */
@@ -17,7 +17,7 @@ float calculatePointDepth(CanvasTriangle triangle, CanvasPoint point) {
     float denominator = (triangle.v1().y-triangle.v2().y)*(triangle.v0().x-triangle.v2().x)+(triangle.v2().x-triangle.v1().x)*(triangle.v0().y-triangle.v2().y);
     float w0 = ((triangle.v1().y-triangle.v2().y)*(point.x-triangle.v2().x)+(triangle.v2().x-triangle.v1().x)*(point.y-triangle.v2().y))/denominator;
     float w1 = ((triangle.v2().y-triangle.v0().y)*(point.x-triangle.v2().x)+(triangle.v0().x-triangle.v2().x)*(point.y-triangle.v2().y))/denominator;
-    float w2 = 1.0 - w0 - w1;
+    float w2 = 1.0f - w0 - w1;
     if (w0 < 0 || w1 < 0 || w2 < 0) {
         return 0; // point is not inside the triangle
     }
