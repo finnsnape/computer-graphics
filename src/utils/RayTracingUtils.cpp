6a1e1b6c8b6b228d2393d5de5449a24e27a97935
finnsnape <snapefinn@gmail.com>
2022-11-29 21:22:01 +0000

    Added solid shadows to ray tracing logic

--- src/utils/RayTracingUtils.cpp
+++ src/utils/RayTracingUtils.cpp
@@ -3,13 +3,12 @@
 #include <CanvasPoint.h>
 #include <ModelTriangle.h>
 #include <RayTriangleIntersection.h>
-#include <Camera.h>
 #include "Scene.h"
 #include "TriangleUtils.h"
 
 namespace {
-    glm::vec3 calculateRawIntersection(Camera &camera, ModelTriangle triangle, glm::mat3 DEMatrix) {
-        glm::vec3 SPVector = camera.position - triangle.vertices[0];
+    glm::vec3 calculateRawIntersection(glm::vec3 from, ModelTriangle triangle, glm::mat3 DEMatrix) {
+        glm::vec3 SPVector = from - triangle.vertices[0];
         glm::vec3 rawIntersection = glm::inverse(DEMatrix) * SPVector;
         return rawIntersection;
     }
@@ -24,7 +23,7 @@ namespace {
         return false;
     }
 
-    glm::vec3 calculateIntersection(ModelTriangle triangle, glm::mat3 DEMatrix, glm::vec3 rawIntersection) {
+    glm::vec3 calculateIntersection(glm::mat3 DEMatrix, ModelTriangle triangle, glm::vec3 rawIntersection) {
         glm::vec3 e0 = DEMatrix[1];
         glm::vec3 e1 = DEMatrix[2];
         glm::vec3 intersectionPoint = triangle.vertices[0] + rawIntersection.y * e0 + rawIntersection.z * e1;
@@ -40,41 +39,61 @@ namespace {
 
     /// @brief Returns the normalised direction vector from the camera position to the point on the canvas, in 3D space
     glm::vec3 calculateRayDirection(Scene &scene, CanvasPoint canvasPoint) {
-        float z = 2.0;
+        // convert canvas point to 3D point in camera coordinate space
+        float z = scene.camera.position.z - scene.camera.focalLength;
         float x = z * (canvasPoint.x - scene.width/2)/(scene.width/2 * scene.camera.focalLength);
         float y = z * (canvasPoint.y - scene.height/2)/(scene.height/2 * -scene.camera.focalLength);
+        // then ray is normalised difference:
+        // note we should do orientation * what we return!
         return glm::normalize(glm::vec3(x, y, z) - scene.camera.position);
     }
 
     /// @brief Returns the triangle that the ray corresponding to the given canvas point first intersects with
-    RayTriangleIntersection findClosestTriangle(Scene &scene, CanvasPoint canvasPoint) {
-        glm::vec3 rayDirection = calculateRayDirection(scene, canvasPoint);
+    RayTriangleIntersection findClosestTriangle(Scene &scene, glm::vec3 rayDirection, glm::vec3 from) {
         RayTriangleIntersection closestTriangle;
         closestTriangle.distanceFromCamera = FLT_MAX;
         for (size_t i=0; i<scene.triangles.size(); i++) {
             ModelTriangle triangle = scene.triangles[i];
             glm::mat3 DEMatrix = calculateDEMatrix(triangle, rayDirection);
             if (glm::determinant(DEMatrix) == 0) continue; // would be dividing by 0
-            glm::vec3 rawIntersection = calculateRawIntersection(scene.camera, triangle, DEMatrix);
+            glm::vec3 rawIntersection = calculateRawIntersection(from, triangle, DEMatrix);
             if (!validateRawIntersection(rawIntersection)) continue;
             if (rawIntersection.x >= closestTriangle.distanceFromCamera) continue; // something closer already exists
-            glm::vec3 intersectionPoint = calculateIntersection(triangle, DEMatrix, rawIntersection);
+            glm::vec3 intersectionPoint = calculateIntersection(DEMatrix, triangle, rawIntersection);
             closestTriangle = {intersectionPoint, rawIntersection.x, triangle, i};
         }
         return closestTriangle;
     }
 
+    bool canSeeLight(Scene &scene, RayTriangleIntersection closestTriangle) {
+        // ray from intersection to light source
+        glm::vec3 rayDirection = glm::normalize(closestTriangle.intersectionPoint - scene.lightSource);
+        // get triangle closest to light source
+        RayTriangleIntersection newClosestTriangle = findClosestTriangle(scene, rayDirection, scene.lightSource);
+        // if triangle we are trying to draw is the triangle closest to the light source
+        // then that means it can see the light. otherwise, it cannot as it's behind another one
+        if (closestTriangle.triangleIndex == newClosestTriangle.triangleIndex) {
+            return true;
+        }
+        return false;
+    }
 }
 
 namespace RayTracingUtils {
-    void draw(Scene &scene) {
+    void draw(Scene &scene, bool shadows) {
         for (int x=0; x<scene.width; x++) {
             for (int y=0; y<scene.height; y++) {
                 CanvasPoint canvasPoint((float) x, (float) y);
-                RayTriangleIntersection closestTriangle = findClosestTriangle(scene, canvasPoint);
-                if (closestTriangle.distanceFromCamera != FLT_MAX) {
-                    TriangleUtils::drawPixel(scene.window, canvasPoint, closestTriangle.intersectedTriangle.colour);
+                glm::vec3 rayDirection = calculateRayDirection(scene, canvasPoint);
+                RayTriangleIntersection closestTriangle = findClosestTriangle(scene, rayDirection, scene.camera.position);
+                if (closestTriangle.distanceFromCamera == FLT_MAX) {
+                    continue; // no triangle intersection found
                 }
+                Colour pixelColour = closestTriangle.intersectedTriangle.colour;
+                if (shadows && !canSeeLight(scene, closestTriangle)) {
+                    pixelColour = {0, 0, 0}; // in shadow
+                }
+                TriangleUtils::drawPixel(scene.window, canvasPoint, pixelColour);
             }
         }
     }
--- src/utils/RayTracingUtils.h
+++ src/utils/RayTracingUtils.h
@@ -3,5 +3,5 @@
 class Scene; // pre-declare to avoid circular dependency
 
 namespace RayTracingUtils {
-    void draw(Scene &scene);
+    void draw(Scene &scene, bool shadows);
 }
