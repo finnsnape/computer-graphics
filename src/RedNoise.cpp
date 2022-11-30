fc3b3e5aa7e70a4694afda02c41d9a1a82bf3d83
finnsnape <snapefinn@gmail.com>
2022-11-30 18:25:13 +0000

    Removed unnecessary imports and cleaned up main file

--- src/RedNoise.cpp
+++ src/RedNoise.cpp
@@ -1,14 +1,10 @@
-#include <DrawingWindow.h>
 #include <CanvasPoint.h>
 #include <ModelTriangle.h>
 #include <glm/glm.hpp>
 #include <vector>
 #include "Camera.h"
 #include "Scene.h"
-#include "TriangleUtils.h"
 #include "FilesUtils.h"
-#include "RayTracingUtils.h"
-#include "RasterisingUtils.h"
 #include "EventUtils.h"
 
 #define WIDTH 240
@@ -25,7 +21,7 @@ void printInstructions() {
     "=======================" << std::endl;
 }
 
-Scene init(Mode mode, float scaleFactor, float focalLength, glm::vec3 initialPosition, glm::vec3 lightSource) {
+Scene initScene(Mode mode, float scaleFactor, float focalLength, glm::vec3 initialPosition, glm::vec3 lightSource) {
     std::string objFileName = "cornell-box.obj";
     std::string mtlFileName = "cornell-box.mtl";
     Camera camera(WIDTH, HEIGHT, focalLength, initialPosition);
@@ -35,15 +31,14 @@ Scene init(Mode mode, float scaleFactor, float focalLength, glm::vec3 initialPos
 }
 
 int main(int argc, char *argv[]) {
-    printInstructions();
-    // TODO: clean this up, maybe just do scene.lightSource = ... etc.
-    Mode mode = WIRE_FRAME; // RASTERISED, RAY_TRACED, WIRE_FRAME, RAY_TRACED_SHADOWS
+    Mode mode = WIRE_FRAME;
     float scaleFactor = 0.35;
     float focalLength = 2.0;
     glm::vec3 lightSource(0.0, 0.35, 0.0);
     glm::vec3 initialPosition(0.0, 0.0, 4.0);
-    Scene scene = init(mode, scaleFactor, focalLength, initialPosition, lightSource);
+    Scene scene = initScene(mode, scaleFactor, focalLength, initialPosition, lightSource);
     SDL_Event event;
+    printInstructions();
     while(true) {
         if (scene.window.pollForInputEvents(event)) EventUtils::handleEvent(event, scene);
         scene.draw();
