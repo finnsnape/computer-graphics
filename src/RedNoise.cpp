e0f9e6a64befa2a01ae0eaaf7bcf09659fbe14c0
finnsnape <snapefinn@gmail.com>
2022-12-09 01:29:12 +0000

    Added logic for render/lighting modes to main file

--- src/RedNoise.cpp
+++ src/RedNoise.cpp
@@ -13,15 +13,17 @@
 void printInstructions() {
     std::cout <<
     "=================================" << std::endl <<
-    "MODES: " << std::endl <<
+    "RENDER MODES: " << std::endl <<
     "1: Wire Frame" << std::endl <<
     "2: Rasterised" << std::endl <<
     "3: Ray Traced" << std::endl <<
-    "4: Ray Traced (Hard Shadows)" << std::endl <<
-    "5: Ray Traced (Proximity Lighting)" << std::endl <<
-    "6: Ray Traced (Angle of Incidence Lighting)" << std::endl <<
     std::endl <<
-    "LIGHT OPERATIONS: " << std::endl <<
+    "LIGHTING MODES (Ray Traced): " << std::endl <<
+    "4: Hard Shadows" << std::endl <<
+    "5: Proximity" << std::endl <<
+    "6: Angle of Incidence" << std::endl <<
+    std::endl <<
+    "LIGHT POSITION OPERATIONS: " << std::endl <<
     "H: Translate (y, positive)" << std::endl <<
     "N: Translate (y, negative)" << std::endl <<
     "B: Translate (x, negative)" << std::endl <<
@@ -29,7 +31,7 @@ void printInstructions() {
     "J: Translate (z, positive)" << std::endl <<
     "K: Translate (z, negative)" << std::endl <<
     std::endl <<
-    "CAMERA OPERATIONS: " << std::endl <<
+    "CAMERA POSITION OPERATIONS: " << std::endl <<
     "O: Orbit (y, positive)" << std::endl <<
     "W: Translate (y, positive)" << std::endl <<
     "S: Translate (y, negative)" << std::endl <<
@@ -44,22 +46,23 @@ void printInstructions() {
     "=================================" << std::endl;
 }
 
-Scene initScene(Scene::Mode mode, float scaleFactor, glm::vec3 initialPosition, glm::vec3 lightSource) {
+Scene initScene(Scene::RenderMode renderMode, Scene::LightingMode lightingMode, float scaleFactor, glm::vec3 initialPosition, glm::vec3 lightSource) {
     std::string objFileName = "cornell-box.obj";
     std::string mtlFileName = "cornell-box.mtl";
     Camera camera(WIDTH, HEIGHT, initialPosition, false);
     std::vector<ModelTriangle> triangles = FilesUtils::loadOBJ(scaleFactor, objFileName, mtlFileName);
-    Scene scene(WIDTH, HEIGHT, mode, triangles, camera, lightSource);
+    Scene scene(WIDTH, HEIGHT, renderMode, lightingMode, triangles, camera, lightSource);
     return scene;
 }
 
 int main(int argc, char *argv[]) {
-    Scene::Mode mode = Scene::RASTERISED;
+    Scene::RenderMode renderMode = Scene::RAY_TRACED;
+    Scene::LightingMode lightingMode = Scene::ANGLE_OF_INCIDENCE;
     float scaleFactor = 0.35;
-    glm::vec3 lightSource(0.0, 0.8, 0.0);
+    glm::vec3 lightSource(0.0, 0.9, 0.7);
     glm::vec3 initialPosition(0.0, 0.0, 4.0);
     Camera camera(WIDTH, HEIGHT, initialPosition, false);
-    Scene scene = initScene(mode, scaleFactor, initialPosition, lightSource);
+    Scene scene = initScene(renderMode, lightingMode, scaleFactor, initialPosition, lightSource);
     SDL_Event event;
     printInstructions();
     scene.draw();
